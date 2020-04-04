4cf9ab98e7b7757ad7c120603e1b5b1d2b907847
Mikołaj Koziarkiewicz <mikolak-net@users.noreply.github.com>
2020-04-04 20:50:58 +0200

    Fixed expand FS check to be more resilient.

--- library/expand_fs.py
+++ library/expand_fs.py
@@ -4,16 +4,21 @@
 
 from ansible.module_utils.basic import *
 
-MAIN_PARTITION_NAME = "/dev/mmcblk0p2"
-
+SD_CARD_PARTITION_NAME = "/dev/mmcblk0p2"
 
 def main():
     module = AnsibleModule(argument_spec=dict())
 
-    (rc, out, err) = module.run_command(["sudo", "raspi-config", "--expand-rootfs"], check_rc=True)
+    (rc, out, err) = module.run_command(["df", "/"], check_rc=True)
+    main_partition_name = out.split("\n")[1].split()[0] #first line is header
 
-    fs_expanded = (lambda x, y: x-y)(*[int(l.split()[2]) for l in out.split("\n") if l.startswith(MAIN_PARTITION_NAME)])
-    module.exit_json(changed=fs_expanded, stdout=out, stderr=err)
+    if main_partition_name != SD_CARD_PARTITION_NAME:
+        module.exit_json(changed=False, stderr="WARN: The root partition {} does not appear to be on an SD card, resize via raspi-config will not work. Doing nothing.".format(main_partition_name))
+    else:
+        (rc, out, err) = module.run_command(["sudo", "raspi-config", "--expand-rootfs"], check_rc=True)
+
+        fs_expanded = (lambda x, y: x-y)(*[int(l.split()[2]) for l in out.split("\n") if l.startswith(main_partition_name)])
+        module.exit_json(changed=fs_expanded, stdout=out, stderr=err)
 
 
 main()
